e80961f06e2e41f134a9e031d4d684da1ec31887
praful <prafulpatnecha@gmail.com>
2024-10-16 19:13:14 +0530

    16-10-24

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -1,18 +1,47 @@
-
-
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 HomeGetController homeGetController = Get.put(HomeGetController());
-class HomeGetController extends GetxController{
+
+class HomeGetController extends GetxController {
+
+
+  HomeGetController()
+  {
+    historyGetMethod();
+  }
+
   RxDouble progress = 0.0.obs;
   RxString linkUrl = "https://www.google.com/search?q=".obs;
-
-  void progressMethed(var value)
-  {
-    progress.value = value/100;
-    
+  RxList<String> historyList = <String>[].obs;
+  void progressMethed(var value) {
+    progress.value = value / 100;
   }
-    void linkUrlMethod(String value)
+
+  void linkUrlMethod(String value) {
+    linkUrl.value = value;
+  }
+
+  Future<void> historySetMethod(String value)
+  async {
+    historyList.add(value);
+    try{  
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    sharedPreferences.setStringList("history", historyList);
+    }catch(e)
     {
-      linkUrl.value = value;
+      print("Problum");
     }
+    // print(historyList);
+  }
+  Future<void> historyGetMethod()
+  async {
+    // try{
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    historyList.value = sharedPreferences.getStringList("history") ?? [];
+    // }catch(e)
+    // {
+    //   print("List Not Found");
+    // }
+  }
 }
--- lib/view/home/home_page.dart
+++ lib/view/home/home_page.dart
@@ -2,6 +2,8 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:get/get.dart';
+import 'package:get/get_common/get_reset.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:my_browser_app/controller/home_controller.dart';
 
@@ -34,6 +36,36 @@ class HomePage extends StatelessWidget {
               children: [
                 IconButton(
                     onPressed: () async {
+                      // final controller = webViewController;
+                      // if (controller != null) {
+                      //   // log(controller.getOriginalUrl().toString());
+                      //   print(homeGetController.historyList);
+                      // }
+                      showModalBottomSheet(
+                        context: context,
+                        builder: (context) {
+                          return ListView.builder(
+                            itemCount: homeGetController.historyList.length,
+                            itemBuilder: (context, index) => ListTile(
+                              onTap: () {
+                                final controller = webViewController;
+                                if(controller!=null)
+                                  {
+                                    controller.loadUrl(urlRequest: URLRequest(url: WebUri(homeGetController.historyList[index])));
+                                  }
+                                Get.back();
+                              },
+                              title: Text("History"),
+                              leading: Icon(Icons.history),
+                              subtitle: Text(homeGetController.historyList[index],maxLines: 1,),
+                            ).paddingOnly(top: (index==0)?20:5),
+                          );
+                        },
+                      );
+                    },
+                    icon: Icon(Icons.history)),
+                IconButton(
+                    onPressed: () async {
                       final controller = webViewController;
                       if (controller != null) {
                         controller.goBack();
@@ -62,9 +94,10 @@ class HomePage extends StatelessWidget {
                       if (controller != null) {
                         try {
                           controller.loadUrl(
-                              urlRequest: URLRequest(
-                                  url: WebUri(
-                                      homeGetController.linkUrl.value),),);
+                            urlRequest: URLRequest(
+                              url: WebUri(homeGetController.linkUrl.value),
+                            ),
+                          );
                         } catch (e) {
                           log("Not Working");
                         }
@@ -83,86 +116,95 @@ class HomePage extends StatelessWidget {
           ],
         ),
         appBar: AppBar(
-          leading: 
-            IconButton(onPressed: () {
-              showMenu(context: context, position: RelativeRect.fromRect(Rect.largest, Rect.zero), items: [
-                CheckedPopupMenuItem(
-                  onTap: () {
-                    final controller =webViewController;
-                    if(controller!=null)
-                    {
-                      String linkString = "https://in.search.yahoo.com/search;_ylt=Awr1QhExoA9nzFQMxyW6HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3F1aWNrbGlua3M-?ei=UTF-8&fr=sfp-fba&fr2=p%3As%2Cv%3Asfp%2Cm%3Afeaturebar&p=";
-                      homeGetController.linkUrlMethod(linkString);
-                      controller.loadUrl(urlRequest: URLRequest(
-                        url: WebUri(linkString),
-                      ));
-                    }
-                  },
-                  child: Text("Yahoo"),
-                ),
-                CheckedPopupMenuItem(
-                  onTap: () {
-                    final controller =webViewController;
-                    if(controller!=null)
-                    {
-                      String linkString = "https://search.brave.com/search?q=";
-                      homeGetController.linkUrlMethod(linkString);
-                      controller.loadUrl(urlRequest: URLRequest(
-                        url: WebUri(linkString),
-                      ));
-                    }
-                  },
-                  child: Text("Brave"),
-                ),
-                CheckedPopupMenuItem(
-                  onTap: () {
-                    final controller =webViewController;
-                    if(controller!=null)
-                    {
-                      String linkString = "https://www.google.com/search?q=";
-                      homeGetController.linkUrlMethod(linkString);
-                      controller.loadUrl(urlRequest: URLRequest(
-                        url: WebUri(linkString),
-                      ));
-                    }
-                  },
-                  child: Text("Google"),
-                ),
-                CheckedPopupMenuItem(
-                  onTap: () {
-                    final controller =webViewController;
-                    if(controller!=null)
-                    {
-                      String linkString = "https://www.bing.com/search?pglt=41&q=";
-                      homeGetController.linkUrlMethod(linkString);
-                      controller.loadUrl(urlRequest: URLRequest(
-                        url: WebUri(linkString),
-                      ));
-                    }
-                  },
-                  child: Text("Edge"),
-                ),
-                CheckedPopupMenuItem(
-                  onTap: () {
-                    final controller =webViewController;
-                    if(controller!=null)
-                    {
-                      String linkString = "https://duckduckgo.com/?t=h_&q=";
-                      homeGetController.linkUrlMethod(linkString);
-                      controller.loadUrl(urlRequest: URLRequest(
-                        url: WebUri(linkString),
-                      ));
-                    }
-                  },
-                  child: Text("Duckduckgo"),
-                ),
-              ]);
-            }, icon: const Icon(Icons.more_vert)),
+          leading: IconButton(
+              onPressed: () {
+                showMenu(
+                    context: context,
+                    position: RelativeRect.fromRect(Rect.largest, Rect.zero),
+                    items: [
+                      CheckedPopupMenuItem(
+                        onTap: () {
+                          final controller = webViewController;
+                          if (controller != null) {
+                            String linkString =
+                                "https://in.search.yahoo.com/search;_ylt=Awr1QhExoA9nzFQMxyW6HAx.;_ylu=Y29sbwNzZzMEcG9zAzEEdnRpZAMEc2VjA3F1aWNrbGlua3M-?ei=UTF-8&fr=sfp-fba&fr2=p%3As%2Cv%3Asfp%2Cm%3Afeaturebar&p=";
+                            homeGetController.linkUrlMethod(linkString);
+                            controller.loadUrl(
+                                urlRequest: URLRequest(
+                              url: WebUri(linkString),
+                            ));
+                          }
+                        },
+                        child: Text("Yahoo"),
+                      ),
+                      CheckedPopupMenuItem(
+                        onTap: () {
+                          final controller = webViewController;
+                          if (controller != null) {
+                            String linkString =
+                                "https://search.brave.com/search?q=";
+                            homeGetController.linkUrlMethod(linkString);
+                            controller.loadUrl(
+                                urlRequest: URLRequest(
+                              url: WebUri(linkString),
+                            ));
+                          }
+                        },
+                        child: Text("Brave"),
+                      ),
+                      CheckedPopupMenuItem(
+                        onTap: () {
+                          final controller = webViewController;
+                          if (controller != null) {
+                            String linkString =
+                                "https://www.google.com/search?q=";
+                            homeGetController.linkUrlMethod(linkString);
+                            controller.loadUrl(
+                                urlRequest: URLRequest(
+                              url: WebUri(linkString),
+                            ));
+                          }
+                        },
+                        child: Text("Google"),
+                      ),
+                      CheckedPopupMenuItem(
+                        onTap: () {
+                          final controller = webViewController;
+                          if (controller != null) {
+                            String linkString =
+                                "https://www.bing.com/search?pglt=41&q=";
+                            homeGetController.linkUrlMethod(linkString);
+                            controller.loadUrl(
+                                urlRequest: URLRequest(
+                              url: WebUri(linkString),
+                            ));
+                          }
+                        },
+                        child: Text("Edge"),
+                      ),
+                      CheckedPopupMenuItem(
+                        onTap: () {
+                          final controller = webViewController;
+                          if (controller != null) {
+                            String linkString =
+                                "https://duckduckgo.com/?t=h_&q=";
+                            homeGetController.linkUrlMethod(linkString);
+                            controller.loadUrl(
+                                urlRequest: URLRequest(
+                              url: WebUri(linkString),
+                            ));
+                          }
+                        },
+                        child: Text("Duckduckgo"),
+                      ),
+                    ]);
+              },
+              icon: const Icon(Icons.more_vert)),
           title: const Text("My Browser"),
           centerTitle: true,
           bottom: PreferredSize(
               preferredSize: Size.fromHeight(50),
-              child: Container(
+              child: SizedBox(
                 child: TextField(
                   onSubmitted: (value) {
                     final controller = webViewController;
@@ -171,7 +213,7 @@ class HomePage extends StatelessWidget {
                         controller.loadUrl(
                           urlRequest: URLRequest(
                             url: WebUri(
-                                homeGetController.linkUrl.value+"$value"),
+                                homeGetController.linkUrl.value + "$value"),
                           ),
                         );
                       } catch (e) {
@@ -184,7 +226,7 @@ class HomePage extends StatelessWidget {
                       label: Text("Search"),
                       border: OutlineInputBorder(
                           borderSide: BorderSide(color: Colors.blue))),
-                ),
+                ).paddingSymmetric(horizontal: 10),
               )),
         ),
         // ignore: deprecated_member_use
@@ -198,8 +240,32 @@ class HomePage extends StatelessWidget {
           onWebViewCreated: (controller) {
             webViewController = controller;
           },
+          // onConsoleMessage: (controller, consoleMessage) {
+          //   log("$controller<-------------------------------------------------------------------------");
+          // },
           // onReceivedError: (controller, request, error) {
 
+          // },
+          onLoadResource: (controller, resource) {},
+          onUpdateVisitedHistory: (controller, url, isReload) async {
+            if ("https://www.google.com/webhp" != url.toString() &&
+                "https://in.search.yahoo.com/?fr=sfp-fba" != url.toString() &&
+                "https://search.brave.com/" != url.toString() &&
+                "https://www.bing.com/?scope=web&cc=IN" != url.toString() &&
+                "https://duckduckgo.com/?t=h_&q=" != url.toString()) {
+              log("->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>(${url.toString()})");
+              // log(url.toString());
+              homeGetController.historySetMethod(url.toString());
+            }
+          },
+          // onTitleChanged: (controller, title) {
+          // if("https://www.google.com/webhp"!=url.toString() && "https://in.search.yahoo.com/?fr=sfp-fba"!=url.toString() && "https://search.brave.com/"!=url.toString() && "https://www.bing.com/?scope=web&cc=IN"!=url.toString() && "https://duckduckgo.com/?t=h_&q="!=url.toString())
+          // {
+          //   homeGetController.historySetMethod(title.toString());
+          //   log("->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>(${title.toString()})");
+          // }
+          // }
+          // log("->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>(${url.toString()})");
           // },
           onProgressChanged: (controller, progress) {
             homeGetController.progressMethed(progress);
